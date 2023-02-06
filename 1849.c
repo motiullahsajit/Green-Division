e689619e11a13175f9b36a8261bcfd45ca34b192
motiullahsajit <sajitmotiullah@gmail.com>
2023-02-06 11:29:11 +0600

    solution updated

--- 1849.c
+++ 1849.c
@@ -1,33 +1,31 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int min(int a, int b)
-{
-    if (a < b)
-        return a;
-    else
-        return b;
-}
-int max(int a, int b)
-{
-    if (a > b)
-        return a;
-    else
-        return b;
+int min(int x, int y){
+    if(x < y) return x;
+    else return y;
 }
 
+int max(int x, int y){
+    if(x > y) return x;
+    else return y;
+}
 
-int main() {
-    int w1, l1, w2, l2, a,b;
+int main(){
 
-    scanf("%d %d %d %d", &w1, &l1, &w2, &l2);
+    int l1,l2,c1,c2,a,b,c;
 
-    a = min(w1, l1);
-    b = min(w2, l2);
+    scanf("%d %d %d %d",&l1, &l2, &c1, &c2);
+
+    a = min(l1,l2);
+    b = min(c1,c2);
+
     a += b;
-    int x = min(max(w1, l1), max(w2, l2));
-    int y = min(a,x);
 
-    printf("%d\n", y*y);
+    c = min(max(l1,l2),max(c1,c2));
 
-    return 0;
+    int e = min(a,c);
+
+    printf("%d\n",e*e);
+
+
 }
--- uri.c
+++ uri.c
@@ -1,19 +1,7 @@
 #include<stdio.h>
 
 int main(){
-    int n,students,total=0;
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        scanf("%d",&students);
 
-        if(students%3 == 0){
-            total += students;
-        }else{
-            total += (students - (students%3));
-        }
-    }
-
-    printf("%d\n", total);
 
     return 0;
 }

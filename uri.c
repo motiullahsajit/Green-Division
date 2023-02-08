3080c627b941701aee0e72489c8ebc54dd8b64e2
motiullahsajit <sajitmotiullah@gmail.com>
2023-02-08 20:51:39 +0600

    solution updated

--- uri.c
+++ uri.c
@@ -1,50 +1,16 @@
 #include<stdio.h>
-#include<math.h>
-
-int check_prime(int n){
-    if(n==0||n==1)return 0;
-    if(n==2) return 1;
-    if(n%2==0)return 0;
-
-    for(int i=3; i<=sqrt(n); i+=2){
-        if(n%i==0){
-           return 0;
-        }
-    }
-
-    return 1;
-}
-
-int is_super_prime(int n){
-    while (n>=10)
-    {
-        int d = n %10;
-        n /= 10;
-        if(check_prime(d) == 0) return 0;
-
-    }
-
-    if (n==2 || n==3 || n==5 || n == 7) return 1;
-    else return 0;
-    
-}
 
 int main(){
 
-    int n;
+    unsigned long long int a, b;
 
-    while (scanf("%d", &n) != EOF)
+    while (scanf("%llu %llu", &a, &b) != EOF)
     {
-       
-        if(check_prime(n) == 0){
-            printf("Nada\n");
-        }else{
-            if(is_super_prime(n) == 1) printf("Super\n");
-            else printf("Primo\n");
-        }
+        if(a > b) printf("%llu\n", a-b);
+        else printf("%llu\n", b-a);
     }
     
-    
+
 
     return 0;
 }
